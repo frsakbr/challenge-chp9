8ff5ea8356fe2bcd5786c5e448a2e3dad4ec991d
faris <frsakbr@gmail.com>
2022-10-18 21:22:33 +0700

    mailer challenge success

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const nodemailer = require('nodemailer')
 
 const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
+    service: 'gmail',
+    host: "smtp.google.com",
     port: 465,
     secure: true,
     auth: {
-        user: "frsakbr@gmail.com",
+        user: "user",
         pass: "password"
-    }
+    },
+    tls: {rejectUnauthorized: false}
+
 })
 
 // format untuk alis -> "alias" <emailnya@gmail.com>
 
 // send email with transporter
 transport.sendMail({
-    from: "frsakbr@gmail.com", // sender address
-    to: "contoh@contih.com", // list of receivers
-    subject: "Submission: Chapter-9 Challenge#<faris>", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+    from: '"faris akbar" <noreply@frsakbr@gmail.com>', // sender address
+    to: "risusanto@outlook.com", // list of receivers
+    subject: "Submission: Chapter-9 Challenge#<faris akbar>", // Subject line
+    text: "Favorite Song: ada apa denganmu | link github: https://github.com/frsakbr/challenge-chp9", // plain text body
+    html: "<b>Favorite Song: ada apa denganmu | link github: https://github.com/frsakbr/challenge-chp9</b>", // html body
 }).then(res => {
     console.log(
         res.messageId
@@ -27,4 +30,3 @@ transport.sendMail({
     console.error(err)
 })
 
-console.log('ini harusnya selesai duluan...')
